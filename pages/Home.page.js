5649828726c4f68d10c3ec36c0a8eaabc6db9ff3
AgustinM12 <agustinmazza12345@gmail.com>
2024-05-05 16:48:48 -0300

    se hicieron correcciones menores a la app

--- pages/Home.page.js
+++ pages/Home.page.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { Button, Card, Text } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useContext } from 'react';
@@ -11,37 +11,46 @@ export function HomeScreen({ navigation }) {
     const LeftContent = props => <MaterialCommunityIcons name="gamepad-square-outline" size={24} color="black" />
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ScrollView style={{ flex: 1 }}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-            {
-                list.length > 0 ? 
-                    list.map((item, key) => (
-                        <Card key={key}>
-                            <Card.Title title={item} left={LeftContent} />
-                            <Card.Content>
-                                <Text variant="titleLarge">Portrait</Text>
-                            </Card.Content>
-                            <Card.Cover source={{ uri: `https://ui-avatars.com/api/?name=${item}&background=random` }} />
-                            <Card.Actions>
-                                <Button>Cancel</Button>
-                                <Button>Ok</Button>
-                            </Card.Actions>
-                        </Card>
-                    )
-                ) : (
-                    <Text>
-                        La lista esta vacia
-                    </Text>
-                )
-            }
+                {
+                    list.length > 0 ?
+                        list.map((item, key) => (
+                            <Card key={key} style={{ paddingBottom: 10 }}>
+                                <Card.Title title={item} left={LeftContent} />
+                                <Card.Content>
+                                    <Text variant="titleLarge">Portrait</Text>
+                                </Card.Content>
+                                <Card.Cover source={{ uri: `https://ui-avatars.com/api/?name=${item}&background=random` }} />
+                                <Card.Actions>
+                                    <Button>Cancel</Button>
+                                    <Button>Ok</Button>
+                                </Card.Actions>
+                            </Card>
+                        )
+                        ) : (
+                            <Text>
+                                La lista esta vacia
+                            </Text>
+                        )
+                }
 
-            <Button
-                style={{ backgroundColor: "purple" }}
-                labelStyle={{ color: "#ffffff" }}
-                onPress={() => navigation.navigate('Add')}
-            >
-                Go to add
-            </Button>
-        </View>
+                <Button
+                    style={{ backgroundColor: "purple" }}
+                    labelStyle={{ color: "#ffffff" }}
+                    onPress={() => navigation.navigate('Add')}
+                >
+                    Go to add
+                </Button>
+                <Button
+                    style={{ backgroundColor: "purple" }}
+                    labelStyle={{ color: "#ffffff" }}
+                    onPress={() => navigation.navigate('Counter')}
+                >
+                    Go to Counter
+                </Button>
+            </View>
+        </ScrollView>
     );
 }
